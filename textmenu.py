c5ba726483811edb7ae09f0b51a3f21987b8f2d2
CaelynMorrison <enkryptx@gmail.com>
2025-02-12 16:45:53 -0500

    implement methods to add and remove menu items

--- textmenu.py
+++ textmenu.py
@@ -1,29 +1,32 @@
 class MenuItem:
-    def __init__(self, display_name: str, function_to_call: str) -> None:
+    def __init__(self, display_name: str, function_to_call: str, index: int) -> None:
         self.display_name = display_name
         self.function_to_call = function_to_call
+        self.index = index
 
     def __str__(self) -> str:
-        return "aaa"
+        return f"{self.index}. {self.display_name.upper()}"
 
 class TextMenu:
     def __init__(self) -> None:
         self.menu_items: dict = {}
 
     def __str__(self) -> str:
-        return f"{self.menu_items}"
+        output = ""
+        for menu_item in self.menu_items:
+            output += f"{self.menu_items[menu_item]}\n"
+        return f"{output}"
+    
+    def next_index(self) -> int:
+        return len(self.menu_items) + 1
 
-    def add_menu_item(self, menu_item: MenuItem) -> None:
-        self.menu_items[menu_item.display_name] = menu_item
-        pass
+    def add_menu_item(self, display_name: str, function_to_call: str) -> None:
+        self.menu_items[display_name] = MenuItem(display_name,
+                                                function_to_call,
+                                                self.next_index())
 
-    def remove_menu_item(self) -> None:
-        pass
+    def remove_menu_item(self, display_name: str) -> None:
+        del self.menu_items[display_name]
 
     def get_user_input(self) -> None:
         pass
-
-main_menu = TextMenu()
-main_menu.add_menu_item(MenuItem("test", "testing"))
-
-print(main_menu)
